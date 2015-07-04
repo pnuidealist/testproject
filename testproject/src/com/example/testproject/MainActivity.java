7588a2bf7c62b88a015b59ea5e48d1d190ad57ae
jjy <jjy@jjytab>
2015-07-04 18:38:27 +0900

    int i = 0;

--- testproject/src/com/example/testproject/MainActivity.java
+++ testproject/src/com/example/testproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int i = 0; // 종엽이 추가함
     }
 
     @Override
